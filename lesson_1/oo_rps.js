163eed4800b9b4132843b9b0c4a5db567db5c0e5
Philip <22357184+philip-33@users.noreply.github.com>
2021-02-06 11:25:19 -0800

    init Obj. Oriented Rock Paper Scissors

---
+++ lesson_1/oo_rps.js
@@ -0,0 +1,126 @@
+/*
+text description of RPS:
+two players pick between the 3 options: rock, paper, scissors
+the winner is determined by the following:
+  both pick the same is a draw.
+  rock beats scissors,
+  paper beats rock,
+  scissors beats paper
+
+Note: This example is artificial,
+and real world OOP problems will have more obvious nouns/verbs
+
+nouns (persons, places, things): 
+  player (2 players), 
+  options (3 moves), 
+  rule (logic for finding the outcome)
+
+verbs (actions):
+  pick (player picks a move/choice)
+  determine (the player choices are compared)
+*/
+
+/*
+OO RPS Bonus Features:
+
+*/
+
+const readline = require('readline-sync');
+
+function createPlayer() {
+  return {
+    move: null,
+  };
+}
+
+function createHuman() {
+  let playerObject = createPlayer();
+
+  let humanObject = {
+    choose() {
+      let choice;
+
+      while (true) {
+        console.log('Please choose rock, paper, or scissors:');
+        choice = readline.question();
+        if (['rock', 'paper', 'scissors'].includes(choice)) break;
+        console.log('Sorry, invalid choice.');
+      }
+      this.move = choice;
+    },
+  };
+
+  return Object.assign(playerObject, humanObject);
+  //Object.assign copies properties from arg1 (source) to arg2 (target)
+}
+
+function createComputer() {
+  let playerObject = createPlayer();
+
+  let computerObject = {
+    choose() {
+      const choices = ['rock', 'paper', 'scissors'];
+      let randomIndex = Math.floor(Math.random() * choices.length);
+      this.move = choices[randomIndex];
+    },
+  };
+
+  return Object.assign(playerObject, computerObject);
+}
+
+const RPSGame = {
+  human: createHuman(),
+  computer: createComputer(),
+
+  displayWelcomeMessage() {
+    console.log('Welcome to RPS!');
+  },
+
+  displayGoodbyeMessage() {
+    console.log('Goodbye from RPS!');
+  },
+
+  displayWinner() {
+    let humanMove = this.human.move;
+    let computerMove = this.computer.move;
+
+    console.log(`You chose: ${this.human.move}`);
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    if (
+      (humanMove === 'rock' && computerMove === 'scissors') ||
+      (humanMove === 'paper' && computerMove === 'rock') ||
+      (humanMove === 'scissors' && computerMove === 'paper')
+    ) {
+      console.log('You win!');
+    } else if (
+      (humanMove === 'rock' && computerMove === 'paper') ||
+      (humanMove === 'paper' && computerMove === 'scissors') ||
+      (humanMove === 'scissors' && computerMove === 'rock')
+    ) {
+      console.log('Computer wins!');
+    } else {
+      console.log("It's a tie");
+    }
+  },
+
+  playAgain() {
+    console.log('Would you like to play again? (y/n)');
+    let answer = readline.question();
+    return answer.toLowerCase()[0] === 'y';
+  },
+
+  play() {
+    this.displayWelcomeMessage();
+    while (true) {
+      this.human.choose();
+      this.computer.choose();
+      this.displayWinner();
+      if (!this.playAgain()) break;
+    }
+
+    this.displayGoodbyeMessage();
+  },
+};
+
+RPSGame.play();
